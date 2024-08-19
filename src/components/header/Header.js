4e4b4f9e387c2a599100a6ee032a87563ae6b755
QentiX <programmisteconomist21@gmail.com>
2024-08-19 22:26:46 +0300

    create: added theme change mode associated with local storage

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import "./style.css";
+import './style.css'
 
 const Header = () => {
 	return (
@@ -6,7 +6,7 @@ const Header = () => {
 			<div className='header__wrapper'>
 				<h1 className='header__title'>
 					<strong>
-						Hi, my name is <em>Yuri</em>
+						Hi, my name is <em>Asta</em>
 					</strong>
 					<br />a frontend developer
 				</h1>
